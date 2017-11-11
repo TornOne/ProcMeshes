c51cdfa4ea83c495b03c2156e24726c8f1f5b8c2
Jaagup Kirme <jaagupkirme@hotmail.com>
2017-11-11 10:53:37 +0200

    One square
    Now I have two triangles. And it works also.

--- Assets/Meshes/jkMesh.cs
+++ Assets/Meshes/jkMesh.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class jkMesh : MonoBehaviour
+public class JKMesh : MonoBehaviour
 {
     public Vector3[] newVertices;
     public Vector2[] newUV;
@@ -12,9 +12,11 @@ public class jkMesh : MonoBehaviour
         Mesh mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
         
-        mesh.vertices = new Vector3[] { new Vector3(0, 0, 0), new Vector3(0, 1, 0), new Vector3(1, 1, 0) };
+        mesh.vertices = new Vector3[] { new Vector3(0, 0, 0), new Vector3(0, 1, 0), new Vector3(1, 1, 0),
+                                        new Vector3(1, 0, 0)};
         mesh.uv = new Vector2[] { new Vector2(0, 0), new Vector2(0, 1), new Vector2(1, 1) };
-        mesh.triangles = new int[] { 0, 1, 2 };
+        mesh.triangles = new int[] { 0, 1, 2,
+                                     0, 2, 3 };
 
     }
     void Update()
