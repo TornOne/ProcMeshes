42f7429df7eade45d4c67b5e3aeeaf7abd7404a2
TornOne <ott.adermann@gmail.com>
2017-11-19 01:19:58 +0200

    Camera controller and first build
    Mouselook + WASD + LShift, LCtrl to move

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public float speed, rotSpeed;
+
+	void Update () {
+		transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("UpDown"), Input.GetAxisRaw("Vertical")) * (Time.deltaTime * speed));
+		transform.localRotation = Quaternion.Euler(transform.localEulerAngles + new Vector3(-Input.GetAxisRaw("Mouse Y"), Input.GetAxisRaw("Mouse X")) * (Time.deltaTime * rotSpeed));
+	}
+}
