185a18641ce692fcbac4d44a91784f57b7478863
TornOne <ott.adermann@gmail.com>
2018-01-04 17:39:30 +0200

    Reorganized

---
+++ Assets/Scripts/Sculpting/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour {
+	public float gravity = 9.80665f;
+	public float speed, rotSpeed;
+	float yVelocity = 0;
+	public GameObject camera;
+	CharacterController controller;
+
+	void Start() {
+		controller = GetComponent<CharacterController>();
+	}
+
+	void Update() {
+		transform.localPosition = new Vector3(transform.localPosition.x, 1, transform.localPosition.z);
+		camera.transform.Rotate(Time.deltaTime * -rotSpeed * Input.GetAxisRaw("Mouse Y"), 0, 0);
+		transform.Rotate(0, Time.deltaTime * rotSpeed * Input.GetAxisRaw("Mouse X"), 0);
+		controller.Move(transform.TransformDirection(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized * speed * (Input.GetButton("Sprint") ? 2 : 1) * Time.deltaTime);
+	}
+}
