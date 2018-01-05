dd3ccbd5b2e42ae548fa03634a06d4b9a5cbfd83
TornOne <ott.adermann@gmail.com>
2018-01-06 01:23:50 +0200

    Fixed bugs, implemented universal add/remove

--- Assets/Scripts/Sculpting/NaiveSculpture.cs
+++ Assets/Scripts/Sculpting/NaiveSculpture.cs
@@ -8,11 +8,14 @@ public class NaiveSculpture : MonoBehaviour {
 	bool[] gridPoints;
 
 	Mesh mesh;
+	MeshCollider coll;
+	float brushSize = 0.1f;
+	bool meshModified = false;
 
 	void Start() {
 		mesh = new Mesh();
+		coll = GetComponent<MeshCollider>();
 		GetComponent<MeshFilter>().mesh = mesh;
-		GetComponent<MeshCollider>().sharedMesh = mesh;
 		mesh.MarkDynamic();
 		gridPoints = new bool[segments.x * segments.y * segments.z];
 		for (int i = 0; i < gridPoints.Length; i++) {
@@ -23,6 +26,26 @@ public class NaiveSculpture : MonoBehaviour {
 
 		transform.localScale = new Vector3(size.x / segments.x, size.y / segments.y, size.z / segments.z);
 		transform.localPosition = new Vector3(size.x * -0.5f, 0, size.z * -0.5f);
+	}
+
+	void Update() {
+		if (Input.GetButtonDown("Fire1")) {
+			RaycastHit hitInfo;
+			if (coll.Raycast(Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f)), out hitInfo, 2)) {
+				Modify(hitInfo.point, brushSize, false);
+			}
+		} else if (Input.GetButtonDown("Fire2")) {
+			RaycastHit hitInfo;
+			if (coll.Raycast(Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f)), out hitInfo, 2)) {
+				Modify(hitInfo.point, brushSize, true);
+			}
+		}
+		brushSize = Mathf.Clamp01(brushSize + Input.GetAxisRaw("Mouse ScrollWheel") * 0.1f);
+
+		if (meshModified) {
+			RefreshMesh();
+			meshModified = false;
+		}
 	}
 
 	void RefreshMesh() {
@@ -129,12 +152,37 @@ public class NaiveSculpture : MonoBehaviour {
 			}
 		}
 
+		mesh.Clear();
 		mesh.vertices = vertices.ToArray();
 		mesh.triangles = triangles.ToArray();
 		mesh.RecalculateNormals();
+		coll.sharedMesh = mesh;
 	}
 
-	void Update() {
-		RefreshMesh();
+	void Modify(Vector3 location, float size, bool add) {
+		location -= transform.localPosition;
+		location = new Vector3(location.x / transform.localScale.x, location.y / transform.localScale.y, location.z / transform.localScale.z);
+		location -= new Vector3(0.5f, 0.5f, 0.5f);
+		Vector3 radius = new Vector3(size / transform.localScale.x, size / transform.localScale.y, size / transform.localScale.z);
+
+		int endY = Mathf.Min((int)(location.y + radius.y), segments.y - 1);
+		int endZ = Mathf.Min((int)(location.z + radius.z), segments.z - 1);
+		int endX = Mathf.Min((int)(location.x + radius.x), segments.x - 1);
+		int layerLength = segments.x * segments.z;
+		for (int y = Mathf.Max(Mathf.CeilToInt(location.y - radius.y), 0); y <= endY; y++) {
+			int yIndex = y * layerLength;
+			for (int z = Mathf.Max(Mathf.CeilToInt(location.z - radius.z), 0); z <= endZ; z++) {
+				int zIndex = z * segments.x;
+				for (int x = Mathf.Max(Mathf.CeilToInt(location.x - radius.x), 0); x <= endX; x++) {
+					Vector3 distance = new Vector3((location.x - x) / radius.x, (location.y - y) / radius.y, (location.z - z) / radius.z);
+					if (Vector3.Dot(distance, distance) <= 1) {
+						gridPoints[yIndex + zIndex + x] = add;
+					}
+				}
+			}
+		}
+
+		meshModified = true;
 	}
+
 }
