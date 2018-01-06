72bfcf057d231a865bdb109bf59769f4ea2de17d
TornOne <ott.adermann@gmail.com>
2018-01-06 21:51:34 +0200

    Final build hopefully

---
+++ Assets/Scripts/Sculpting/SceneSwitcher.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour {
+	GameObject player;
+	public string scene;
+
+	void Start() {
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	void OnMouseDown() {
+		if (Vector3.Distance(player.transform.position, transform.position) < 2) {
+			SceneManager.LoadScene(scene);
+		}
+	}
+}
